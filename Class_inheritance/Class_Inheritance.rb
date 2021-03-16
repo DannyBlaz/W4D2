8120554df9ec6148727f55c2637d123c1545f3b3
Joseph Kim <jkjoeeekim@live.com>
2021-03-16 10:50:52 -0700

    Finish Class_Inheritance

--- Class_inheritance/Class_Inheritance.rb
+++ Class_inheritance/Class_Inheritance.rb
@@ -17,8 +17,13 @@ end
 class Manager < Employee
     attr_reader :employees
 
-    def initialize
+    def initialize(name, title, salary, boss)
+        super
         @employees = []
+    end
+
+    def add_employee(employee)
+        self.employees << employee
     end
 
     def bonus(multiplier)
@@ -28,3 +33,18 @@ class Manager < Employee
     end
     
 end
+
+# p ned = Manager.new("Ned", "Founder", 1000000, nil)
+# p darren = Manager.new("Darren", "TA Manager", 78000, "Ned")
+# p shawna = Employee.new("Shawna", "TA", 12000, "Darren")
+# p david = Employee.new("David", "TA", 10000, "Darren")
+# darren.add_employee(shawna)
+# darren.add_employee(david)
+
+# p darren.bonus(4)
+# p david.bonus(3)
+
+# ned.add_employee(darren)
+# ned.add_employee(shawna)
+# ned.add_employee(david)
+# p ned.bonus(5)
