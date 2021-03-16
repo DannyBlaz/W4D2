07434aa8dbec71726058e98fe4b9526852e40d9f
Daniel <dannyblaze469@gmail.com>
2021-03-16 14:18:17 -0400

    Added skeleton files

---
+++ Chess_(Part_One)/Piece.rb
@@ -0,0 +1,43 @@
+
+class Piece
+    attr_reader :location
+
+    def initialize(location = nil)
+        @location = location
+
+    end
+    
+end
+
+class NullPiece < Piece
+
+    
+    
+end
+
+class Sliding
+
+    def initialize(location)
+        super
+
+    end
+    
+end
+
+class Stepping
+
+    def initialize(location)
+        super
+
+    end
+    
+end
+
+class Pawn
+
+    def initialize(location)
+        super
+
+    end
+    
+end
